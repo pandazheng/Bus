5523c4d2879c18a5ee02a51fc3aa49a678ab419b
panda zheng <pandazheng@pandas-MacBook-Pro.local>
2013-04-22 07:57:02 -0700

    Initial Commit

---
+++ Bus/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Bus
+//
+//  Created by panda zheng on 13-4-22.
+//  Copyright (c) 2013年 panda zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Bus/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Bus
+//
+//  Created by panda zheng on 13-4-22.
+//  Copyright (c) 2013年 panda zheng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
